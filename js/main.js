a20cfb5d292a0919441c74c37265ef0237a79df6
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-27 21:13:52 +0200

    He cambiado una foto de mi header y he corregido el código con las urls de mi const header images para que las imagenes puedan cambiar aleatoriamente de forma correcta

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // 1. Mi proyecto
 const headerImages = [
-    'assets/imagenes/1.jpg',
-    'assets/imagenes/2.jpg',
-    'assets/imagenes/3.jpg'
+    {url: 'assets/imagenes/1.jpg', alt: 'La puesta de sol' },
+    {url: 'assets/imagenes/2.jpg', alt: 'Globos volando en el cielo azul'},
+    {url: 'assets/imagenes/6.jpg', alt: 'Montes nevados'},
 ];
 
 const galleryData = [
-    { url: 'assets/imagenes/viajes-1.jpg', desc: 'Una mujer descansando en una hamaca mirando hacia el mar.' },
-    { url: 'assets/imagenes/viajes-2.jpg', desc: 'Un camino hacia un bungalow en las Maldivas.' },
-    { url: 'assets/imagenes/viajes-3.jpg', desc: 'El cielo azul con señales de destinos turísticos de várias ciudades' },
-    { url: 'assets/imagenes/viajes-4.jpg', desc: 'La plaza de España en Sevilla.' },
-    { url: 'assets/imagenes/viajes-7.jpg', desc: 'Un castillo de el medio de un pueblo en España.' },
-    { url: 'assets/imagenes/viajes-6.jpg', desc: 'Un puente junto a acantilado.' }
+    { url: 'assets/imagenes/viajes-1.jpg', desc: 'Una mujer descansando en una hamaca frente el mar.' },
+    { url: 'assets/imagenes/viajes-2.jpg', desc: 'Un puente sobre el mar que lleva a un bungalow en las Maldivas.' },
+    { url: 'assets/imagenes/viajes-3.jpg', desc: 'El cielo azul con señales de destinos turísticos de várias ciudades.' },
+    { url: 'assets/imagenes/viajes-4.jpg', desc: 'La plaza de España de Sevilla.' },
+    { url: 'assets/imagenes/viajes-7.jpg', desc: 'Un castillo en el medio de un pueblo en España.' },
+    { url: 'assets/imagenes/viajes-6.jpg', desc: 'Un puente junto a un acantilado con un cielo nublado.' }
 ];
 
 // Función principal que se ejecuta al cargar la página
@@ -24,7 +24,8 @@ window.onload = function() {
     const randomIndex = Math.floor(Math.random() * headerImages.length);
     
     // URL aleatoria al src del header
-    headerImgElement.src = headerImages[randomIndex];
+    headerImgElement.src = headerImages[randomIndex].url;
+    headerImgElement.alt = headerImages[randomIndex].alt;
 
     // GALERÍA DINÁMICA
     const galleryContainer = document.getElementById('gallery-container');
